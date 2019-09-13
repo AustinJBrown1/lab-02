122b357469fea6325a8f670934e96640fdcf0545
Austin <oct.16.00@gmail.com>
2019-09-13 11:22:29 -0700

    Changed Css

--- js/app.js
+++ js/app.js
@@ -1,62 +1,91 @@
-'use strict';
+// 'use strict';
 
-var name = prompt('What is your name?')
-   alert('Hello ' + name +', welcome to my webpage!')
-   alert('Now that introductions are in order, would you like to take a short quiz?')
+// var name = prompt('What is your name?')
+//    alert('Hello ' + name +', welcome to my webpage!')
+//    alert('Now that introductions are in order, would you like to take a short quiz?')
 
-var answerTown = prompt('Am I from a town in texas? (y/n)').toLowerCase();
+// var answerTown = prompt('Am I from a town in texas? (y/n)').toLowerCase();
 
-if(answerTown === 'yes' || answerTown === 'y')
-{ 
-    alert('Right, its a lovely place right north of Dallas !');
-    console.log('User guessed correctly');
-}else{
-    alert('Wrong, its a town north of Dallas.');console.log('User guessed incorrectly')};
+// function town() {
+// if(answerTown === 'yes' || answerTown === 'y'){ 
+//   alert('Right, its a lovely place right north of Dallas !');
+//   console.log('Yeehaw, pardner!');
+// }else{
+//   alert('Wrong, its a town north of Dallas.');
+//   console.log('What in tarnation?!');
+//   }
+// }
+// town()
 
-var answerBirthDay = prompt('Is my birth day in october? (y/n)').toLowerCase();
+// var answerBirthDay = prompt('Is my birth day in october? (y/n)').toLowerCase();
 
-if(answerBirthDay === 'yes' || answerBirthDay === 'y')
-{ alert('Yes, my birthday is on the sixtenth of october!');console.log('User guessed correctly');}
-else{
-alert('No my birth is indeed in october.');console.log('User did not guess correctly')};
+// function birthDay(){
+//   if(answerBirthDay === 'yes' || answerBirthDay === 'y'){ 
+//     alert('Yes, my birthday is on the sixtenth of october!');
+//     console.log('User guessed correctly');
+//   }else{
+//     alert('No my birth is indeed in october.');
+//     console.log('User did not guess correctly');
+//   }
+// }
+// birthDay();
+// var answerWork = prompt('Did I work at walmart as a cashier before coming to code fellows? (y/n)').toLowerCase();
 
-var answerWork = prompt('Did I work at walmart as a cashier before coming to code fellows? (y/n)').toLowerCase();
-if(answerWork === 'yes' || answerWork === 'y')
-{ alert('Yes I worked there for about nine months before coming to seattle');console.log('User guessed correctly');}
-else{
-alert('If only you were right about that!');console.log('User did not guess correctly')};
+// function work(){
+//   if(answerWork === 'yes' || answerWork === 'y'){ 
+//     alert('Yes I worked there for about nine months before coming to seattle');
+//     console.log('User guessed correctly');
+//   }else{
+//   alert('If only you were right about that!');console.log('User did not guess correctly');
+//   }
+// }
+// work()
+// var answerSchool = prompt('Did I graduate highschool in may of 2019? (y/n)').toLowerCase();
 
-var answerSchool = prompt('Did I graduate highschool in may of 2019? (y/n)').toLowerCase();
-if(answerSchool === 'yes' || answerSchool === 'y')
-{ alert('Yes, I did and I am so glad to be done with it!');console.log('User guessed correctly');}
-else{
-alert('I would be very sad if you were right.');console.log('User did not guess correctly')};
+// function school(){
+//   if(answerSchool === 'yes' || answerSchool === 'y'){ 
+//     alert('Yes, I did and I am so glad to be done with it!');
+//     console.log('User guessed correctly');
+//   }else{
+//     alert('I would be very sad if you were right.');
+//     console.log('User did not guess correctly');
+//   }
+// }
+// school()
 
-var answerMagic = prompt('Do I love the collectable trading card game Magic the Gathering? (y/n)').toLowerCase();
-if(answerMagic === 'yes' || answerMagic === 'y')
-{ alert('Yes, its one of my favorite games, you try it!');console.log('User guessed correctly');}
-else{
-alert('That is indeed the wrong answer, ' + name + '.' + ' Magic is one of the best games ever made, you should try it.');console.log('User did not guess correctly.')};
+// var answerMagic = prompt('Do I love the collectable trading card game Magic the Gathering? (y/n)').toLowerCase();
 
-var numAnswer = parseInt(prompt('Guess a number'));
-var counter = 0;
+// function magic(){
+//   if(answerMagic === 'yes' || answerMagic === 'y'){ 
+//     alert('Yes, its one of my favorite games, you try it!');console.log('User guessed correctly');
+//   }else{
+//   alert('That is indeed the wrong answer, ' + name + '.' + ' Magic is one of the best games ever made, you should try it.');
+//   console.log('User did not guess correctly.')
+//   }
+// } 
+// magic()
 
-for(var i = 0; i < 3; i++){
-    if(numAnswer === 7){
-        alert('Correct');
-        console.log()
-        break;   
-    }else if(numAnswer < 7){
-        numAnswer = prompt('Too low, try again')
-        console.log()
-        counter++
-    }else if(numAnswer > 7){
-        numAnswer = prompt('Too high, try again')
-        console.log()
-        counter++
-    }    
-}
-if(counter === 3){
-    alert('The answer is 7')
-}
+// var numAnswer = parseInt(prompt('Guess a number'));
+// var counter = 0;
 
+// function numGuess(){
+//   for(var i = 0; i < 3; i++){
+//     if(numAnswer === 7){
+//       alert('Correct');
+//       console.log()
+//       break;   
+//     }else if(numAnswer < 7){
+//       numAnswer = prompt('Too low, try again')
+//       console.log()
+//       counter++
+//     }else if(numAnswer > 7){
+//       numAnswer = prompt('Too high, try again')
+//       console.log()
+//       counter++
+//     }    
+//   }
+//   if(counter === 3){
+//     alert('The answer is 7')
+//   }
+// }
+// numGuess()
